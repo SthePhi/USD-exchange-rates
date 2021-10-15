946b475c1768baee37f014971ba81fdab1489c72
SthePhi <37987676+SthePhi@users.noreply.github.com>
2021-10-15 05:46:02 +0200

    first commit

---
+++ src/app/currency.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {throwError} from "rxjs";
+import {retry, catchError} from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CurrencyService {
+
+  private CURRENCY_URL = "https://freecurrencyapi.net/api/v2/latest";
+
+  constructor(private http: HttpClient) { }
+
+  handleError(error: HttpErrorResponse){
+    let errorMessage = 'Unknown error!';
+    if (error.error instanceof ErrorEvent){
+      errorMessage = 'Error: ${error.error.message}';
+    } else{
+      errorMessage = 'Error Code: ${error.status}\nMessage: ${error.message}';
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+  }
+
+  public getCurrency(){
+    return this.http.get(this.CURRENCY_URL).pipe(retry(3), catchError(this.handleError));
+  }
+}
