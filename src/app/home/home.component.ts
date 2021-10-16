7ab970de9694c85f6eae5a8a61be32eeafdc737c
SthePhi <37987676+SthePhi@users.noreply.github.com>
2021-10-17 01:09:15 +0200

    updated crrencies display

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {CurrencyService} from "../currency.service";
 import {Currency} from "../currency";
+import {formatCurrency} from "@angular/common";
 
 @Component({
   selector: 'app-home',
@@ -9,18 +10,20 @@ import {Currency} from "../currency";
 })
 export class HomeComponent implements OnInit {
 
-  currencies: any;
+  bdt: any;
+  inr: any;
+  pkr: any;
+  zar: any;
 
   constructor(private currencyService: CurrencyService) { }
 
   ngOnInit(): void {
     this.currencyService.getCurrency().subscribe((data:Currency)=>{
-      // console.log(data);
-
-      this.currencies = data;
-      switch (this.currencies) {
-      }
-      console.log(this.currencies.constructor);
+      console.log(data.data);
+      this.bdt = data.data.BDT;
+      this.inr = data.data.INR;
+      this.pkr = data.data.PKR;
+      this.zar = data.data.ZAR;
     })
   }
 
